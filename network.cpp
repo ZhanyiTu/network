f42f4100a411d555e56784a489a664410750f90a
tomtu <125548913@qq.com>
2019-11-08 15:57:57 +0800

    看函数写了一个版本 然后开始看代码

--- network.cpp
+++ network.cpp
@@ -1,11 +1,34 @@
 ﻿// network.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
-
+#include <Winsock.h>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    WORD wVersionRequested;//WORD结构的版本号
+    WSADATA wsa;
+    int err;
+
+
+    if (WSAStartup(MAKEWORD(1, 1), &wsa) != 0) {//加载Winsock库
+        return;
+    }
+
+    SOCKET Listen_Sock = socket(AF_INET, SOCK_STREAM, 0);//构建监听socket 流式socket 服务端socket
+    SOCKET Client_Sock = socket(AF_INET, SOCK_STREAM, 0);//构建通讯socket 流式socket 客户端socket
+
+    SOCKADDR_IN serverAddr;//配置监听地址和端口
+    ZeroMemory((char*)&serverAddr, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(12001);//端口设置
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);//有IP
+    bind(Listen_Sock, (struct sockaddr*) & serverAddr, sizeof(serverAddr));//服务端绑定监听socket
+
+    int len = sizeof(SOCKADDR);
+    SOCKET Command_Sock = accept(Listen_Sock, (SOCKADDR*)&serverAddr, &len);//还有这种类型转换?是因为只是求一个地址的地址吧 长度居然直接使用sizeof就可以求出来了
+
+
+    WSACleanup();//释放Winsock库
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
