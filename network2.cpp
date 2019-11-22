4a3c5b1bf58d67f55523746a3b525952c029eed2
tomtu <125548913@qq.com>
2019-11-22 16:34:06 +0800

    问题原来是我没有使用htons将主机字节顺序转换为网络字节顺序 所以端口一直不对

--- network2.cpp
+++ network2.cpp
@@ -26,8 +26,8 @@ int main()
     SOCKET sockSrv = socket(AF_INET, SOCK_STREAM, 0);//流式socket 源socket
 
     SOCKADDR_IN addrSrv;//源socket的地址
-    //addrSrv.sin_addr.S_un.S_addr = htonl(INADDR_ANY);//#define INADDR_ANY              (ULONG)0x00000000 参数是Host地址 默认设置为0
-    inet_pton(AF_INET, "127.5.5.5", (void*)&addrSrv.sin_addr);
+    addrSrv.sin_addr.S_un.S_addr = htonl(INADDR_ANY);//#define INADDR_ANY              (ULONG)0x00000000 参数是Host地址 默认设置为0
+    //inet_pton(AF_INET, "127.5.5.5", (void*)&addrSrv.sin_addr);
     /*
     u_long
 WSAAPI
@@ -36,22 +36,29 @@ htonl(
     ); 只看得到函数的参数将一个数转换为Ulong类型
     */
     addrSrv.sin_family = AF_INET;
-    addrSrv.sin_port = 8080;
+    addrSrv.sin_port = htons(8080);
+    memset(addrSrv.sin_zero, 0, 8);
 
     bind(sockSrv, (SOCKADDR*)&addrSrv, sizeof(SOCKADDR));//将socket和地址绑定
 
     listen(sockSrv, 5);//开始监听, 最多监听5个端口
 
     SOCKADDR_IN addrClient;
-    int len = sizeof(SOCKADDR);
+    int len = sizeof(sockaddr);
     while (1) {
-        SOCKET sockConn = accept(sockSrv, (SOCKADDR*)&addrClient, &len);//通过监听功能 接受请求 然后创建会话socket 用于通信
+        SOCKET sockConn = accept(sockSrv, (struct sockaddr*)&addrClient, &len);//通过监听功能 接受请求 然后创建会话socket 用于通信
+        if (sockConn == INVALID_SOCKET) {
+            printf("%d\n", WSAGetLastError());
+        }
         char sendBuf[50];
         char str[50];
+        printf("say something");
         sprintf_s(sendBuf, 50, "Welcome addr %s to here!",inet_ntop(AF_INET, &addrClient.sin_addr, str, sizeof(str)));
-        send(sockConn, sendBuf, strlen(sendBuf) + 1, 0);//通过会话socket 发送数据sendbuf
+        if (send(sockConn, sendBuf, strlen(sendBuf) + 1, 0) <= 0)//通过会话socket 发送数据sendbuf
+            printf("%d\n", WSAGetLastError());
         char recvBuf[50];
-        recv(sockConn, recvBuf, 50, 0);//通过会话socket 返回接受数据recvBuf
+        int sign2 = recv(sockConn, recvBuf, 50, 0);//通过会话socket 返回接受数据recvBuf
+        WSAGetLastError();
         printf("%s\n", recvBuf);//在控制台打印recvBuf
         closesocket(sockConn);//关闭会话socket
     }
